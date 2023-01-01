dcb16e68ca5e01da3663c343ecfed0e69acabb75
RexBanner6000 <tghaynes@gmail.com>
2023-01-01 22:29:46 +0000

    Added linear rgb function

--- src/common.py
+++ src/common.py
@@ -2,13 +2,16 @@ import os
 import rasterio
 import re
 
+import numpy as np
 from pathlib import Path
+from typing import Set
 
 
 class Sentinel2A:
     def __init__(self, path: Path):
         self.path = path
         self.images = self.get_image_files()
+        self.mask: np.ndarray = None
 
     def get_image_files(self):
         img_regex = re.compile(
@@ -23,6 +26,36 @@ class Sentinel2A:
             if not images.get(group[1]):
                 images[group[1]] = {}
             if not images[group[1]].get(group[2]):
-                images[group[1]][group[2]] = group[0]
+                images[group[1]][group[2]] = group[0] + ".jp2"
 
         return images
+
+    def create_mask(self, bands: Set[int] = (4, 5)):
+        scl = rasterio.open(
+            os.path.join(self.path, self.images["SCL"]["20m"])
+        ).read(1)
+
+        mask = np.zeros(scl.shape)
+
+        for band in bands:
+            mask[scl == band] = 1
+
+        return mask
+
+    def create_linear_rgb(self, resolution: str = "10m"):
+        bands = {}
+        for band in ["B04", "B03", "B02"]:
+            bands[band] = rasterio.open(
+                os.path.join(self.path, self.images["B04"][resolution])
+            )
+
+        linear_rgb = np.zeros((bands["B04"].height, bands["B04"].width, 3))
+
+        for i, band in enumerate(bands.values()):
+            linear_rgb[..., i] = band.read(1)
+
+        linear_rgb /= 4000
+
+        return linear_rgb
+
+
