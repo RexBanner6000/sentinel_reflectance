215dc09b5ee3b9c11b1f621f6a37ceb2d11fb7db
RexBanner6000 <tghaynes@gmail.com>
2023-01-07 09:18:09 +0000

    Added test_get_season

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -3,3 +3,7 @@ from src.common import get_season, Season
 
 def test_get_season():
     assert get_season("2022-01-01", 50) == Season.WINTER
+    assert get_season("2022-01-01", -50) == Season.SUMMER
+    assert get_season("2022-04-01", 50) == Season.SPRING
+    assert get_season("2022-07-01", 50) == Season.SUMMER
+    assert get_season("2022-10-01", 50) == Season.AUTUMN
